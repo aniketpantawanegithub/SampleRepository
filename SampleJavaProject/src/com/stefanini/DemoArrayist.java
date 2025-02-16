0ec6ff869bf5e87638f86df0806bff03248deefe
Aniket Pantawane <pantawaneaniket@gmail.com>
2025-02-17 01:14:26 +0530

    Code for ArralList

---
+++ SampleJavaProject/src/com/stefanini/DemoArrayist.java
@@ -0,0 +1,18 @@
+package com.stefanini;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DemoArrayist {
+
+	public static void main(String[] args) {
+		
+		List<String> list = Arrays.asList("ajay","sagar","laksh","amar","jyoti");
+
+		for(String s :list) {
+			System.out.println(s);
+		}
+		
+	}
+
+}
